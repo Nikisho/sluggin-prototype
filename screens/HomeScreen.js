5e9f7058ba9de142be14f5577d7278e0a01cb0d4
Nicholas <phobereromain.gmail.com>
2022-10-23 21:24:01 +0100

    Added react-native-elements search button and SearchBoxes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,77 @@
 import { StyleSheet, Text, Sa, SafeAreaView, View, TextInput, TouchableOpacity } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { Button, Icon } from 'react-native-elements';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`justify-center bg-green-300 h-full`}>
-      <View style={tw`mx-5 bg-white h-1/5 rounded-2xl`}>
+      <View style={tw`mx-7 bg-white rounded-2xl`}>
 
         {/* textInputFrom */}
-        <TouchableOpacity style={tw`h-1/2`}>
-
-        </TouchableOpacity>
-
+        <GooglePlacesAutocomplete 
+            styles={{
+                container: {
+                    flex: 0,
+                },
+                textInput: {
+                    fontSize: 19,
+                },
+            }}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            enablePoweredByContainer={false}
+            query={{
+                key: GOOGLE_MAPS_APIKEY,
+                language: "en"
+            }}
+            placeholder='where from?'
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+        />
         {/* textInputWhereTo */}
-        <TouchableOpacity style={tw`h-1/2 border-t`}>
-            
-        </TouchableOpacity>
+        <GooglePlacesAutocomplete 
+            styles={{
+                container: {
+                    flex: 0,
+                    borderTopWidth: 0.3,
+                    borderTopColor: "gray",
+                },
+                textInput: {
+                    fontSize: 18,
+                },
+            }}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            enablePoweredByContainer={false}
+            query={{
+                key: GOOGLE_MAPS_APIKEY,
+                language: "en"
+            }}
+            placeholder='where to?'
+            nearbyPlacesAPI='GooglePlacesSearch'
+            debounce={400}
+
+        />
+        <Button
+            title="Search"
+            titleStyle={{
+                fontWeight: 'semibold',
+                fontSize: 20
+            }}
+            containerStyle={{
+                height: 50,
+                backgroundColor: 'black',
+            }}
+            buttonStyle={{
+                borderBottomEndRadius: 5,
+                backgroundColor: 'black'
+            }}
+        />
       </View>
     </SafeAreaView>
   )
