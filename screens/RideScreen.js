31f841a8fe975f44d0d944b3af250a77cbfe219e
Nicholas <phobereromain@gmail.com>
2023-03-02 07:59:12 +0000

    added MyRidesScreen component and extra button in homepage. Changed checkmart colour

--- screens/RideScreen.js
+++ screens/RideScreen.js
@@ -192,11 +192,9 @@ const RideScreen = () => {
                         width: 100,
                         height: 100,
                         borderRadius: 100,
-                        backgroundColor: 'lightgreen',
+                        backgroundColor: '#78b13f',
+
                     }}>
-                    {/* <Text style={{ fontSize: 28, textAlign: 'center', margin: 10 }}>
-                        tick
-                    </Text> */}
                     <Icon
                         name='checkmark-outline'
                         type='ionicon'
