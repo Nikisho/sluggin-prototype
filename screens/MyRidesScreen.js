f5042fcaa17fbcb6db638d255c5695c7e82ccd34
Nicholas <phobereromain@gmail.com>
2023-03-06 22:27:11 +0000

    Worked out id mapping and added UserRIde tab, fixed tab problem.

--- screens/MyRidesScreen.js
+++ screens/MyRidesScreen.js
@@ -2,61 +2,77 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
-import { Icon } from '@rneui/base'
-import { Tab } from '@rneui/themed';
+import { Icon, Tab, TabView } from '@rneui/themed'
 import { useNavigation } from '@react-navigation/native'
+import RideOptionsCard from '../components/RideOptionsCard'
+import UserRides from '../components/UserRides'
 
 const MyRidesScreen = () => {
     const navigation = useNavigation();
     const [index, setIndex] = React.useState(0);
 
     return (
-        <SafeAreaView>
-            <View style={tw`mx-2 rounded-xl justify-center items-center h-12 bg-black`}>
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('HomeScreen')}
-                    style={tw`absolute left-4 z-50 rounded-full shadow-lg`}>
-                    <Icon name='chevron-back-outline'
-                        type='ionicon'
-                        color='white'
-                    />
-                </TouchableOpacity>
-                <Text style={tw`text-xl text-white`}>
-                    Your rides
-                </Text>
-            </View>
-            <View style={tw`p-2 rounded-lg`}>
-                <Tab 
-                    
-                    value={index}
-                    onChange={(e) => setIndex(e)}
-                    indicatorStyle={{
-                        backgroundColor: 'black',
-                        height: 1,
-                        
+        <>
+            <SafeAreaView>
+                <View style={tw`mx-2 rounded-xl justify-center items-center h-12 bg-black`}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('HomeScreen')}
+                        style={tw`absolute left-4 z-50 rounded-full shadow-lg`}>
+                        <Icon name='chevron-back-outline'
+                            type='ionicon'
+                            color='white'
+                        />
+                    </TouchableOpacity>
+                    <Text style={tw`text-xl text-white`}>
+                        Your rides
+                    </Text>
+                </View>
+            </SafeAreaView>
+            <Tab
+
+                value={index}
+                onChange={(e) => setIndex(e)}
+                indicatorStyle={{
+                    backgroundColor: 'black',
+                    height: 2,
+
+                }}
+                containerStyle={{
+                    borderRadius: 10,
+                    margin: 8
+                }}
+            >
+                <Tab.Item
+                    title="You ride"
+                    titleStyle={{ fontSize: 12, color: 'black' }}
+                    icon={{ name: 'person', type: 'ionicon', color: 'black' }}
+                    containerStyle={{
+                        backgroundColor: 'white',
+                        borderRadius: 5
                     }}
-                >
-                    <Tab.Item
-                        title="You ride"
-                        titleStyle={{ fontSize: 12, color: 'black' }}
-                        icon={{ name: 'person', type: 'ionicon', color: 'black' }}
-                        containerStyle={{
-                            backgroundColor: 'white',
-                            borderRadius: 5
-                        }}
-                    />
-                    <Tab.Item
-                        title="You drive"
-                        titleStyle={{ fontSize: 12, color: 'black' }}
-                        icon={{ name: 'car', type: 'ionicon', color: 'black' }}
-                        containerStyle={{
-                            backgroundColor: 'white',
-                            borderRadius: 5
-                        }}
-                    />
-                </Tab>
-            </View>
-        </SafeAreaView>
+                />
+                <Tab.Item
+                    title="You drive"
+                    titleStyle={{ fontSize: 12, color: 'black' }}
+                    icon={{ name: 'car', type: 'ionicon', color: 'black' }}
+                    containerStyle={{
+                        backgroundColor: 'white',
+                        borderRadius: 5
+                    }}
+                />
+            </Tab>
+            <TabView value={index} onChange={setIndex} animationType="spring"  
+
+            >
+                    <TabView.Item style={{ backgroundColor: 'white', width: '100%' }}>
+                        <UserRides/>
+                    </TabView.Item>
+                    <TabView.Item style={{ backgroundColor: 'white', width: '100%' }}>
+                        <Text h1>Your Drive</Text>
+                    </TabView.Item>
+           
+            </TabView>
+        </>
     )
 }
 
