3ca25fd99f1c10264617c8f3d4a14928e1056f8b
Nicholas <phobereromain@gmail.com>
2023-03-10 22:57:18 +0000

    added bg image and blurred it

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,72 +4,82 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from '@rneui/base'
 import { SocialIcon } from "@rneui/themed";
+import { loginScreenImage } from '../images/images'
+
 const LoginScreen = () => {
-  const image = {uri: 'https://github.com/Nikisho/sluggin-prototype/blob/development/images/pexels-photo-1051072.webp'};;
-  
+  const image = loginScreenImage;
+
+  const signUpWithGoogle = ()=> {
+    try {
+      console.log('sign up successful!')
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <SafeAreaView style={tw`h-full`}>
-       <ImageBackground source={image} resizeMode="cover" style={tw`h-full`} >
-
-       
-      <View style={tw`items-center p-3`}>
-        <Text style={tw`font-bold text-2xl`}>
-          Start riding now!
-        </Text>
-      </View>
-      <View style={tw`items-center top-1/2 mx-5`}>
-
-        <TouchableOpacity style={tw.style(` p-3 flex-row py-3 mb-1 items-center rounded-full bg-black w-full justify-between `)}>
-          <SocialIcon
-            type='google'
-            iconSize={14}
-            style={{
-              padding: 0,
-              paddingHorizontal: 0
-            }}
-
-          />
-          <Text style={tw`text-white font-bold`}>Sign up with Google</Text>
-          <View style={tw`px-5`}>
-
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={tw.style(` p-3 flex-row py-3 mb-1 items-center rounded-full bg-black w-full justify-between `)}>
-          <SocialIcon
-            type='facebook'
-            iconSize={14}
-            style={{
-              padding: 0,
-              paddingHorizontal: 0
-            }}
-
-          />
-          <Text style={tw`text-white font-bold`}>Sign up with Facebook</Text>
-          <View style={tw`px-5`}>
-
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={tw.style(` px-6 flex-row py-5 rounded-full bg-black w-full justify-between `)}>
-
-          <Icon
-            type='ionicon'
-            name='mail'
-            size={24}
-            color='white'
-          />
+      <ImageBackground source={image} resizeMode="cover" style={tw` h-full w-full`} blurRadius={3} >
 
 
-          <Text style={tw`font-bold text-white `}>
-            Continue with email
+        <View style={tw`items-center p-3`}>
+          <Text style={tw`font-bold text-2xl`}>
+            Start riding now!
           </Text>
+        </View>
+        <View style={tw`items-center top-1/2 mx-5`}>
+
+          <TouchableOpacity style={tw.style(` p-3 flex-row py-3 mb-1 items-center rounded-full bg-black w-full justify-between `)}>
+            <SocialIcon
+              type='google'
+              iconSize={14}
+              style={{
+                padding: 0,
+                paddingHorizontal: 0
+              }}
+
+            />
+            <Text style={tw`text-white font-bold`}>Sign up with Google</Text>
+            <View style={tw`px-5`}>
+
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={tw.style(` p-3 flex-row py-3 mb-1 items-center rounded-full bg-black w-full justify-between `)}>
+            <SocialIcon
+              type='facebook'
+              iconSize={14}
+              style={{
+                padding: 0,
+                paddingHorizontal: 0
+              }}
+
+            />
+            <Text style={tw`text-white font-bold`}>Sign up with Facebook</Text>
+            <View style={tw`px-5`}>
+
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={tw.style(` px-6 flex-row py-5 rounded-full bg-black w-full justify-between `)}>
+
+            <Icon
+              type='ionicon'
+              name='mail'
+              size={24}
+              color='white'
+            />
 
 
-          <View style={tw`px-3`}>
-          </View>
+            <Text style={tw`font-bold text-white `}>
+              Continue with email
+            </Text>
 
-        </TouchableOpacity>
-      </View>
+
+            <View style={tw`px-3`}>
+            </View>
+
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     </SafeAreaView>
   )
